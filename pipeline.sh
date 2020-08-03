bbfdd2021484446134f21c9a74c5793e51b1ec89
preethir903 <60954753+preethir903@users.noreply.github.com>
2020-08-02 19:44:44 -0700

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -143,3 +143,12 @@ docker-compose exec presto presto --server presto:8080 --catalog hive --schema d
 #select count(*) from asset_prices;
 
 #docker-compose down 
+
+
+#SQL Queries
+
+#show how much money user deposited / withdrew by username 
+select event_type, 
+REGEXP_EXTRACT(description, "(.*) (.*)", 1) as user_id,
+REGEXP_EXTRACT(description, "(.*) (.*)", 2) as money
+from cash_transactions
